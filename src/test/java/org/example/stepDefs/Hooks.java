8ab4dab975b5cd1c9b9ac28c617b58d04b77662f
alaa-wali <alaa.a.wali1994@gmail.com>
2024-12-16 20:58:25 +0200

    project submission

---
+++ src/test/java/org/example/stepDefs/Hooks.java
@@ -0,0 +1,27 @@
+package org.example.stepDefs;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Hooks {
+    public static WebDriver driver;
+
+    @Before
+    public void openBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demoblaze.com/");
+    }
+
+    @After
+    public void quitDriver() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+}
